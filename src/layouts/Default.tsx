1f337e87113eac4772cfe9806cfa91739be428d7
unknown <Rick-Sanches2020@yandex.ru>
2025-03-15 12:12:04 +0300

    telegram test

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,31 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import { useEffect } from 'react'
-import { backButton } from '@telegram-apps/sdk'
 
 import DesctopHeadNav from '@/components/Layouts/DesctopHeadNav'
 import LogoHeader from '@/components/Layouts/LogoHeader'
 
 
 const DefaultLayout = () => {
-
-    useEffect(
-        () => {
-            backButton.show()
-
-            const handleBackButtonClick = () => {
-                window.Telegram.WebApp.close()
-            }
-
-            const offClick = backButton.onClick(handleBackButtonClick)
-
-            return () => {
-                backButton.hide()
-                offClick()
-            }
-        },
-        [backButton]
-    )
-
     const userAgent = navigator.userAgent.toLowerCase()
 
     const predMobile  = userAgent.includes('iphone') || userAgent.includes('android')
