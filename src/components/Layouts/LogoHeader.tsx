907af34e07441293a842f4a882a37e42662a8569
unknown <Rick-Sanches2020@yandex.ru>
2025-03-15 11:54:09 +0300

    telegram test

--- src/components/Layouts/LogoHeader.tsx
+++ src/components/Layouts/LogoHeader.tsx
@@ -2,22 +2,13 @@ import SvgLogoTextColor from '@/assets/icon/logo-text-color.svg?react'
 
 
 const LogoHeader = () => {
-    const userAgent = navigator.userAgent.toLowerCase()
-
-    const predMobile  = userAgent.includes('iphone') || userAgent.includes('android')
-    const predDesktop = userAgent.includes('windows') || userAgent.includes('macintosh') || userAgent.includes('win')
-    const isMobile  = predMobile || !predDesktop
-
     return (
         <>
             <div
                 className="logo-header"
                 id="logo-header"
-                style={{
-                    top: isMobile ? '1rem' : '0',
-                }}
             >
-                <SvgLogoTextColor />
+                <SvgLogoTextColor className="logo-img" />
             </div>
         </>
     )
