52337047d2a61fa0f7ec46cfee53a39cf66c6ef5
unknown <Rick-Sanches2020@yandex.ru>
2025-03-18 00:56:42 +0300

    изменение дизайна

--- src/components/AppPreloader.tsx
+++ src/components/AppPreloader.tsx
@@ -1,16 +1,24 @@
-import SvgPreloaderLogo from '@/assets/icon/preloader-logo.svg'
+import SvgPreloaderHeader from '@/assets/icon/preloader-header.svg'
+import SvgPreloaderFooter from '@/assets/icon/preloader-footer.svg'
+import SvgWhiteHeart from '@/assets/icon/white-heart.svg'
+import Svg3Date from '@/assets/icon/3date.svg'
 
 
 const AppPreloader = () => {
     return (
         <>
             <div id="preloader">
-                <div className="preloader__ctx">
-                    <img src={ SvgPreloaderLogo } alt="PreloaderLogo" />
-                </div>
-                <div className="preloader__text">
-                    <span className="loader"></span>
-                    <h6>Загрузка...</h6>
+                <div className="preloader__box">
+                    <header className="preloader__header">
+                        <img className="preloader-img" src={ SvgPreloaderHeader } alt="preloader-header" />
+                    </header>
+                    <main className="preloader__main">
+                        <img src={ SvgWhiteHeart } alt="white-heart" />
+                        <img src={ Svg3Date } alt="3Date" />
+                    </main>
+                    <footer className="preloader__footer">
+                        <img className="preloader-img" src={ SvgPreloaderFooter } alt="preloader-footer" />
+                    </footer>
                 </div>
             </div>
         </>
