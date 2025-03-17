3591e2c7f98a15171e2860c798cbfc9e4995db3a
Terrorick2020 <ilataraskin475@gmail.com>
2025-03-18 00:30:25 +0300

    work with workflow

--- src/components/Admin/Change.tsx
+++ src/components/Admin/Change.tsx
@@ -25,10 +25,10 @@ const ChangeContent = () => {
     return (
         <>
             <div className="change__ctx">
-                <h3 className="headline">Выбор режима!</h3>
+                <h3 className="headline">Выбор режима!!</h3>
                 <div className="links">
                     <NavLink className="link" to={ toPreview }>
-                        <Button className="link__btn" variant="contained">Приложение!</Button>
+                        <Button className="link__btn" variant="contained">Приложение!!</Button>
                     </NavLink>
                     <NavLink className="link" to={ toUsersList }>
                         <Button className="link__btn text-block" variant="contained">Админ!!</Button>
