691f1a258f963eae47e418450a015b810fdc3e2c
unknown <Rick-Sanches2020@yandex.ru>
2025-03-29 11:21:52 +0300

    bugfix

--- src/components/Register/FillingQuest/Media.tsx
+++ src/components/Register/FillingQuest/Media.tsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Collapse from '@mui/material/Collapse';
 
-
-const icon = (
-    <Paper sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
-      <svg>
-        <Box
-          component="polygon"
-          points="0,100 50,00, 100,100"
-          sx={(theme) => ({
-            fill: theme.palette.common.white,
-            stroke: theme.palette.divider,
-            strokeWidth: 1,
-          })}
-        />
-      </svg>
-    </Paper>
-);
 
 interface PropsFillingQuestMedia {
     show: boolean
