31bbbee3a5a13ef767f9982e84197f4f00d5fd65
unknown <Rick-Sanches2020@yandex.ru>
2025-03-29 12:14:26 +0300

    сделал видео

--- src/components/Register/FillingQuest/Media.tsx
+++ src/components/Register/FillingQuest/Media.tsx
@@ -1,4 +1,3 @@
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Collapse from '@mui/material/Collapse';
 
@@ -12,20 +11,21 @@ const FillingQuestMedia = (props: PropsFillingQuestMedia) => {
     return (
         <>
             <div className="video__media">
-                <Collapse in={props.show}>
-                    <Paper sx={{ m: 1, width: 100, height: 100 }} elevation={4}>
-                        <svg>
-                            <Box
-                                component="polygon"
-                                points="0,100 50,00, 100,100"
-                                sx={(theme) => ({
-                                    fill: theme.palette.common.white,
-                                    stroke: theme.palette.divider,
-                                    strokeWidth: 1,
-                                })}
-                            />
-                        </svg>
-                    </Paper>
+                <Collapse in={props.show} orientation="vertical">
+                    <Collapse in={props.show} orientation="horizontal">
+                        <Paper sx={{ width: '100vw', height: '100vh' }} elevation={6}>
+                            <div className="container">
+                                <h4 className="headline">Для выхода нажмите на экран</h4>
+                                <iframe
+                                    className="video"
+                                    src="https://www.youtube.com/embed/7K2sOZDEZ4A"
+                                    title="YouTube video player"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                ></iframe>
+                            </div>
+                        </Paper>
+                    </Collapse>
                 </Collapse>
             </div>
         </>
