9d954bc63299ca191b4938262ed14f0a419fca72
unknown <Rick-Sanches2020@yandex.ru>
2025-03-29 13:15:12 +0300

    настройки tg

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { viewport, init, isTMA, swipeBehavior  } from '@telegram-apps/sdk';
+import { viewport, init, isTMA, swipeBehavior } from '@telegram-apps/sdk';
 
 import store from './store';
 
@@ -22,34 +22,12 @@ async function initTg() {
       await viewport.requestFullscreen();
     }
 
-    // await swipeBehavior.disableVertical();
-  }
-}
-
-function ensureDocumentIsScrollable() {
-  const isScrollable =
-    document.documentElement.scrollHeight > window.innerHeight;
-  if (!isScrollable) {
-    document.documentElement.style.setProperty(
-      "height",
-      "calc(100vh + 1px)",
-      "important"
-    );
-  }
-}
-
-function preventCollapse() {
-  if (window.scrollY === 0) {
-    window.scrollTo(0, 1);
+    await swipeBehavior.disableVertical();
   }
 }
 
 
 (async () => { await initTg() })();
-
-const scrollableElement = document.querySelector(".scrollable-element");
-if ( scrollableElement ) scrollableElement.addEventListener("touchstart", preventCollapse);
-window.addEventListener("load", ensureDocumentIsScrollable);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
