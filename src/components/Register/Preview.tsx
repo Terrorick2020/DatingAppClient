a1c49c49347b5089085672acfe1a0345e850f886
unknown <Rick-Sanches2020@yandex.ru>
2025-05-20 12:51:45 +0300

    head-nav bugfix

--- src/components/Register/Preview.tsx
+++ src/components/Register/Preview.tsx
@@ -25,7 +25,7 @@ const PreviewContent = (): JSX.Element => {
             const logoHeader = document.getElementById('logo-header');
             if( logoHeader ) logoHeader.style.display = 'none';
         },
-        [] 
+        []
     )
 
     const location = useLocation();
@@ -44,7 +44,10 @@ const PreviewContent = (): JSX.Element => {
                         Настоящие встречи начинаются здесь
                         <img className="relative" src={ SvgWhiteHeart } alt="white-heart" />
                     </h1>
-                    <h6 className="description">Мы верим, что крепкие связи устанавливаются лицом к лицу. Наша цель — помочь вам встретить новых людей офлайн, создавая значимые моменты и впечатления.</h6>
+                    <h6 className="description">
+                        Мы верим, что крепкие связи устанавливаются лицом к лицу.
+                        Наша цель — помочь вам встретить новых людей офлайн, создавая значимые моменты и впечатления.
+                    </h6>
                 </div>
                 <div className="btn">
                     <NavLink className="link" to={ toFillQuest } onClick={handleRoute}>
@@ -56,4 +59,4 @@ const PreviewContent = (): JSX.Element => {
     )
 }
 
-export default PreviewContent
+export default PreviewContent;
