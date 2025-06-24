f5ae46c3a67fbb643f4b7627f0665d1dfe1b9659
Аноним Haker <Rick-Sanches2020@yandex.ru>
2025-06-24 12:28:07 +0300

    bugfix

--- src/constant/psych.ts
+++ src/constant/psych.ts
@@ -2,19 +2,17 @@ import { ELineStatus } from '@/types/store.types';
 import type { PsychListItem, TargerPsych, TargerPsychExpListItem } from '@/types/psych.types';
 
 import PngLeady from '@/assets/img/leady.png';
-import PngWoman from '@/assets/img/woman.png';
-import PngFemale from '@/assets/img/female.png';
 
 
 export const psychTestList: PsychListItem[] = [
-    { id: '12434230', avatar: PngWoman, name: 'Михаил', spec: 'Психоаналитик', lineStat: ELineStatus.Online, exp: 8 },
+    { id: '12434230', avatar: PngLeady, name: 'Михаил', spec: 'Психоаналитик', lineStat: ELineStatus.Online, exp: 8 },
     { id: '11434231', avatar: PngLeady, name: 'Анна', spec: 'Когнитивно-поведенческий терапевт', lineStat: ELineStatus.Offline, exp: 5 },
-    { id: '10434232', avatar: PngWoman, name: 'Дмитрий', spec: 'Гештальт-терапевт', lineStat: ELineStatus.Online, exp: 4 },
+    { id: '10434232', avatar: PngLeady, name: 'Дмитрий', spec: 'Гештальт-терапевт', lineStat: ELineStatus.Online, exp: 4 },
     { id: '13434233', avatar: PngLeady, name: 'Екатерина', spec: 'Психотерапевт', lineStat: ELineStatus.Online, exp: 11 },
-    { id: '14434234', avatar: PngWoman, name: 'Алексей', spec: 'Клинический психолог', lineStat: ELineStatus.Offline, exp: 23 },
-    { id: '15434235', avatar: PngFemale, name: 'Мария', spec: 'Детский психолог', lineStat: ELineStatus.Offline, exp: 1 },
+    { id: '14434234', avatar: PngLeady, name: 'Алексей', spec: 'Клинический психолог', lineStat: ELineStatus.Offline, exp: 23 },
+    { id: '15434235', avatar: PngLeady, name: 'Мария', spec: 'Детский психолог', lineStat: ELineStatus.Offline, exp: 1 },
     { id: '16434236', avatar: PngLeady, name: 'Иван', spec: 'Травматерапевт', lineStat: ELineStatus.Offline, exp: 5 },
-    { id: '17434237', avatar: PngFemale, name: 'Светлана', spec: 'Нейропсихолог', lineStat: ELineStatus.Online, exp: 7 },
+    { id: '17434237', avatar: PngLeady, name: 'Светлана', spec: 'Нейропсихолог', lineStat: ELineStatus.Online, exp: 7 },
 ];
 
 const expListValues: TargerPsychExpListItem[] = [
@@ -26,12 +24,12 @@ const expListValues: TargerPsychExpListItem[] = [
 ];
 
 export const targerPsychList: TargerPsych[] = [
-    { id: '12434230', photo: PngWoman, name: 'Михаил', spec: 'Психоаналитик', lineStat: ELineStatus.Online, exp: 8, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
+    { id: '12434230', photo: PngLeady, name: 'Михаил', spec: 'Психоаналитик', lineStat: ELineStatus.Online, exp: 8, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
     { id: '11434231', photo: PngLeady, name: 'Анна', spec: 'Когнитивно-поведенческий терапевт', lineStat: ELineStatus.Offline, exp: 5, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
-    { id: '10434232', photo: PngWoman, name: 'Дмитрий', spec: 'Гештальт-терапевт', lineStat: ELineStatus.Online, exp: 4, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
+    { id: '10434232', photo: PngLeady, name: 'Дмитрий', spec: 'Гештальт-терапевт', lineStat: ELineStatus.Online, exp: 4, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
     { id: '13434233', photo: PngLeady, name: 'Екатерина', spec: 'Психотерапевт', lineStat: ELineStatus.Online, exp: 11, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
-    { id: '14434234', photo: PngWoman, name: 'Алексей', spec: 'Клинический психолог', lineStat: ELineStatus.Offline, exp: 23, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
-    { id: '15434235', photo: PngFemale, name: 'Мария', spec: 'Детский психолог', lineStat: ELineStatus.Offline, exp: 1, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
+    { id: '14434234', photo: PngLeady, name: 'Алексей', spec: 'Клинический психолог', lineStat: ELineStatus.Offline, exp: 23, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
+    { id: '15434235', photo: PngLeady, name: 'Мария', spec: 'Детский психолог', lineStat: ELineStatus.Offline, exp: 1, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
     { id: '16434236', photo: PngLeady, name: 'Иван', spec: 'Травматерапевт', lineStat: ELineStatus.Offline, exp: 5, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
-    { id: '17434237', photo: PngFemale, name: 'Светлана', spec: 'Нейропсихолог', lineStat: ELineStatus.Online, exp: 7, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
+    { id: '17434237', photo: PngLeady, name: 'Светлана', spec: 'Нейропсихолог', lineStat: ELineStatus.Online, exp: 7, desc: 'Выберите любого специалиста и опишите ему свою проблему или попросите совет. Специалист ответит вам в свободное время, а диалог появится в общем чате.', expList: expListValues },
 ];
