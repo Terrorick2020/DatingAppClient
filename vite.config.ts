d826d66122b14ed1b7da4cfc55c92c4ec145e35a
Аноним Haker <Rick-Sanches2020@yandex.ru>
2025-06-27 00:19:43 +0300

    optimize

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig, loadEnv } from 'vite';
 
 import removeConsole from 'vite-plugin-remove-console';
+import preload from 'vite-plugin-preload';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
@@ -31,6 +32,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       svgr(),
+      preload(),
       ViteImageOptimizer({
         png: { quality: 70 },
         jpeg: {
