66eb47711fe72de042e2f989586fbdebd678ba1c
unknown <Rick-Sanches2020@yandex.ru>
2025-10-14 13:39:17 +0300

    bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -136,59 +136,59 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
-    build: {
-      minify: 'terser',
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-        },
-        format: {
-          comments: false,
-        },
-      },
-      // rollupOptions: {
-      //   output: {
-      //     manualChunks(id: string) {
-      //       // if (id.includes('node_modules')) {
+    // build: {
+    //   minify: 'terser',
+    //   terserOptions: {
+    //     compress: {
+    //       drop_console: true,
+    //       drop_debugger: true,
+    //     },
+    //     format: {
+    //       comments: false,
+    //     },
+    //   },
+    //   // rollupOptions: {
+    //   //   output: {
+    //   //     manualChunks(id: string) {
+    //   //       // if (id.includes('node_modules')) {
 
-      //       //   if (id.includes('react-router-dom') || id.includes('react-router')) {
-      //       //     return 'react-router';
-      //       //   };
+    //   //       //   if (id.includes('react-router-dom') || id.includes('react-router')) {
+    //   //       //     return 'react-router';
+    //   //       //   };
  
-      //       //   if (
-      //       //     id.includes('@reduxjs/toolkit') ||
-      //       //     id.includes('react-redux')      ||
-      //       //     id.includes('reselect')
-      //       //   ) {
-      //       //     return 'redux';
-      //       //   };
+    //   //       //   if (
+    //   //       //     id.includes('@reduxjs/toolkit') ||
+    //   //       //     id.includes('react-redux')      ||
+    //   //       //     id.includes('reselect')
+    //   //       //   ) {
+    //   //       //     return 'redux';
+    //   //       //   };
 
-      //       //   if (id.includes('socket.io-client')) {
-      //       //     return 'socket';
-      //       //   };
+    //   //       //   if (id.includes('socket.io-client')) {
+    //   //       //     return 'socket';
+    //   //       //   };
 
-      //       //   if (
-      //       //     id.includes('axios') ||
-      //       //     id.includes('dayjs') ||
-      //       //     id.includes('uuid')
-      //       //   ) {
-      //       //     return 'utils';
-      //       //   };
+    //   //       //   if (
+    //   //       //     id.includes('axios') ||
+    //   //       //     id.includes('dayjs') ||
+    //   //       //     id.includes('uuid')
+    //   //       //   ) {
+    //   //       //     return 'utils';
+    //   //       //   };
 
-      //       //   if (id.includes('@telegram-apps')) {
-      //       //     return 'web-sdk';
-      //       //   };
+    //   //       //   if (id.includes('@telegram-apps')) {
+    //   //       //     return 'web-sdk';
+    //   //       //   };
 
-      //       //   if (id.includes('emoji-mart') || id.includes('@emoji-mart')) {
-      //       //     return 'emoji';
-      //       //   };
+    //   //       //   if (id.includes('emoji-mart') || id.includes('@emoji-mart')) {
+    //   //       //     return 'emoji';
+    //   //       //   };
 
-      //       //   return 'vendor';
-      //       // }
-      //     }
-      //   }
-      // }
-    }
+    //   //       //   return 'vendor';
+    //   //       // }
+    //   //     }
+    //   //   }
+    //   // }
+    // }
   }
 })
