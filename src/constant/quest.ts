9145f93ccd3a9132f2c83ea6925791d636bd5506
unknown <Rick-Sanches2020@yandex.ru>
2025-10-14 13:22:15 +0300

    bugfix

--- src/constant/quest.ts
+++ src/constant/quest.ts
@@ -1,6 +1,6 @@
-export const initialArgs = { limit: 15, offset: 0 }
+export const initialArgs = { limit: 15, offset: 0 };
 
-export const SWIPED_OFFSET = 100
-export const PSYCH_VIDEO_ADD_MARK = 'new'
+export const SWIPED_OFFSET = 100;
+export const PSYCH_VIDEO_ADD_MARK = 'new';
 
-export const CARUSEL_ANIME_MS = 500
+export const CARUSEL_ANIME_MS = 500;
